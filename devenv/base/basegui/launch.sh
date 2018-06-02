cd58a4047692ba9d25fdca9ecd4e5aceb0809d84
Trim Bresilla <trim.bresilla@gmail.com>
2018-06-02 20:05:14 +0200

    devenv changes

--- devenv/base/basegui/launch.sh
+++ devenv/base/basegui/launch.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-x11vnc -create -env FD_PROG=/usr/bin/mate-session -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 -env X11VNC_CREATE_GEOM=${1:-1920x1080x16} -gone 'killall Xvfb' -bg -nopw & opt/noVNC/utils/launch.sh --vnc localhost:5900
+#x11vnc -create -env FD_PROG=/usr/bin/mate-session -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 -env X11VNC_CREATE_GEOM=${1:-1920x1080x16} -gone 'killall Xvfb' -bg -nopw & opt/noVNC/utils/launch.sh --vnc localhost:5900
+
+/opt/websockify/run 5901 --cert=/self.pem --ssl-only --web=/opt/noVNC --wrap-mode=ignore &
+vncserver :1 -securitytypes otp -otp -noxstartup &
+chmod -f 777 /tmp/.X11-unix
+touch ~/.Xauthority && xauth generate :0 . trusted
+/opt/TurboVNC/bin/vncserver -SecurityTypes None
+/opt/noVNC/utils/launch.sh --vnc localhost:5901 --cert /self.pem --listen 40001
